83091ff8462b5d3459dcc0047e50c062aa7b9d23
Erik Hjelm Fjeldheim <erikhfj@stud.ntnu.no>
2026-02-26 22:01:33 +0100

    chore: migrate middleware.ts to proxy.ts for Next.js 16

--- proxy.ts
+++ proxy.ts
@@ -1,23 +1,17 @@
 import createMiddleware from 'next-intl/middleware';
 import { locales } from './i18n/request';
 
-const middleware = createMiddleware({
-    // A list of all locales that are supported
+const proxy = createMiddleware({
     locales: locales,
-
-    // Used when no locale matches
     defaultLocale: 'nb',
-
-    // Don't use a prefix for the default locale
     localePrefix: 'as-needed'
 });
 
 export default function (req: any) {
-    console.log('[middleware] Processing request:', req.nextUrl.pathname);
-    return middleware(req);
+    console.log('[proxy] Processing request:', req.nextUrl.pathname);
+    return proxy(req);
 }
 
 export const config = {
-    // Match only internationalized pathnames
     matcher: ['/', '/(nb|en|de|fr|es)/:path*', '/((?!api|_next|_vercel|.*\\..*).*)']
 };
